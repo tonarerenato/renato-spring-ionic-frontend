31525f272d5638031e138a75be3cb8c0f38be991
tonarerenato <tonarerenato@yahoo.com.br>
2020-04-09 09:17:47 -0300

    Mostrando dados e imagem do cliente para página de profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,8 +20,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Profile', component: "ProfilePage" },
-      { title: 'Categorias', component: "CategoriasPage" }
+      { title: 'Profile', component: 'ProfilePage' },
+      { title: 'Categorias', component: 'CategoriasPage' }
       
     ];
 
--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { StorageService } from '../../services/storage.service';
+import { ClienteDTO } from '../../models/cliente.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
+import { Api_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -9,19 +12,39 @@ import { StorageService } from '../../services/storage.service';
 })
 export class ProfilePage {
 
-  email: string;
+  cliente: ClienteDTO;
 
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
-    public storage: StorageService) {
+    public storage: StorageService,
+    public clienteService: ClienteService) {
   }
 
   ionViewDidLoad() {
     let localUser = this.storage.getLocalUser();
     if (localUser && localUser.email){
-        this.email = localUser.email;
+       this.clienteService.findByEmail(localUser.email)
+       .subscribe(response =>{
+         this.cliente = response;
+         this.getImageIfExists();
+       },
+       error => {});
     }
+  }
+
+  getImageIfExists() {
+    this.clienteService.getImageFromBucket(this.cliente.id)
+    .subscribe(response => {
+      this.cliente.imageUrl = `${Api_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      //this.blobToDataURL(response).then(dataUrl => {
+        //let str : string = dataUrl as string;
+       // this.profileImage = this.sanitizer.bypassSecurityTrustUrl(str);
+      
+    },
+    error => {
+      //this.profileImage = 'assets/imgs/avatar-blank.png';
+    });
   }
 
 }
