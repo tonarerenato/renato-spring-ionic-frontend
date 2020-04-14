b1ed5dabfe5e87a38a6e3d4e7141a0584267a548
tonarerenato <tonarerenato@yahoo.com.br>
2020-04-14 16:43:35 -0300

    carregando imagem dos produtos

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -12,4 +12,9 @@ export class ProdutoService {
   findByCategoria(categoria_id : string) {
     return this.http.get(`${Api_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
   }
+
+  getSmallImageFromBucket(id : string) : Observable<any> {
+    let url = `${Api_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`
+    return this.http.get(url, {responseType : 'blob'});
+  }  
 }
